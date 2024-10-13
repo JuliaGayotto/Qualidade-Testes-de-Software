53f8d2f442ffa472cc54ce517e43d1536e9761af
JuliaGayotto <juliagayotto@gmail.com>
2024-10-13 16:59:05 -0300

    Alteração dos nomes das pessoas nos testes

--- TDD/ex1/tests/personDAO_test.py
+++ TDD/ex1/tests/personDAO_test.py
@@ -19,32 +19,32 @@ def test_valid_person(dao):
     assert len(errors) == 0
 
 def test_invalid_name(dao):
-    person = Person(id=1, name="Júlia", age=21, emails=[Email(1, "julia@gmail.com")])
+    person = Person(id=2, name="João", age=21, emails=[Email(1, "joao@gmail.com")])
     errors = dao.isValidToInclude(person)
     assert "O nome deve ser composto por ao menos 2 partes e deve possuir apenas letras." in errors
 
 def test_invalid_name2(dao):
-    person = Person(id=1, name="Júlia@", age=21, emails=[Email(1, "julia@gmail.com")])
+    person = Person(id=3, name="Lucas @", age=21, emails=[Email(1, "lucas@gmail.com")])
     errors = dao.isValidToInclude(person)
     assert "O nome deve ser composto por ao menos 2 partes e deve possuir apenas letras." in errors
 
 def test_invalid_age(dao):
-    person = Person(id=1, name="Júlia Gayotto", age=300, emails=[Email(1, "julia@gmail.com")])
+    person = Person(id=4, name="Lara Silva", age=300, emails=[Email(1, "lara@gmail.com")])
     errors = dao.isValidToInclude(person)
     assert "A idade deve estar entre 1 e 200." in errors
 
 def test_missing_email(dao):
-    person = Person(id=1, name="Júlia Gayotto", age=21, emails=[])
+    person = Person(id=5, name="Bruno Alves", age=21, emails=[])
     errors = dao.isValidToInclude(person)
     assert "A pessoa deve ter pelo menos um email associado a ela." in errors
 
 def test_invalid_email_format(dao):
-    person = Person(id=1, name="Júlia Gayotto", age=21, emails=[Email(1, "juliagmail.com")])
+    person = Person(id=6, name="Leonardo Sousa", age=21, emails=[Email(1, "leonardogmail.com")])
     errors = dao.isValidToInclude(person)
     assert "O email juliagmail.com não está no formato válido ____@____.____." in errors
 
 def test_totally_invalid_person(dao):
-    person = Person(id=1, name="Júlia 1", age=300, emails=[Email(1, "juliagmail.com")])
+    person = Person(id=7, name="Gabriela 1", age=300, emails=[Email(1, "gabrielagmail.com")])
     errors = dao.isValidToInclude(person)
     assert 'O nome deve ser composto por ao menos 2 partes e deve possuir apenas letras.' in errors
     assert 'A idade deve estar entre 1 e 200.' in errors
